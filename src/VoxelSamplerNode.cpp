9bee756be9c1da1f6b97512a67d4fcce2f225634
Jose Esteve <jose@joesfer.com>
2012-01-02 12:26:49 +0000

    [!] upgraded license

--- src/VoxelSamplerNode.cpp
+++ src/VoxelSamplerNode.cpp
@@ -1,6 +1,6 @@
 /* 
 	================================================================================
-	Copyright (c) 2011, Jose Esteve. http://www.joesfer.com
+	Copyright (c) 2012, Jose Esteve. http://www.joesfer.com
 	All rights reserved. 
 
 	Redistribution and use in source and binary forms, with or without modification, 
