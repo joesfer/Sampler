dd9fdd3868ff0b75786308f3b7077874ee6719dc
Jose Esteve <jose@joesfer.com>
2012-01-02 18:05:41 +0000

    Clarified license terms. Cleared source code headers

--- src/tga.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-bool LoadTGA( const char *file, int& width, int& height, int& colormode, bool BGR2RGB, unsigned char** image );		// Load a tga image.      
-bool WriteTGA( int width, int height, const unsigned char* image, bool BGR2RGB, const char *file ); // Write a tga file.      
-
-
